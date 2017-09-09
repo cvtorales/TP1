667e170d2559c32a2213fff2d92d21d75c8dc469
fedefaganelli <31784079+fedefaganelli@users.noreply.github.com>
2017-09-09 16:24:20 -0300

    Add files via upload

---
+++ messages.h
@@ -0,0 +1,12 @@
+#define SYMBOL_MENU_PERSONAL_REGISTER 'R'
+#define SYMBOL_MENU_ASSIGNATURE 'A'
+#define SYMBOL_MENU_CALC 'M'
+#define SYMBOL_FINALIZE '!'
+#define SYMBOL_QUIT '0'
+#define SYMBOL_SUBMENU_ASSIG_ADD_ASSIG '+'
+#define SYMBOL_SUBMENU_ASSIG_BACK '0'
+
+#define MSG_WELCOME "Bienvenidx"
+
+#define MAX_ROW 10
+#define MAX_COL 30
