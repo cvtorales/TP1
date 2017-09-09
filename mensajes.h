627b98ee574be4a67c60940f6fda82c003db193e
fedefaganelli <31784079+fedefaganelli@users.noreply.github.com>
2017-09-09 16:21:40 -0300

    Delete mensajes.h
    borro este mensajes.h y subo el actualizado

--- mensajes.h
+++
@@ -1,7 +0,0 @@
-#define SYMBOL_MENU_PERSONAL_REGISTER 'R'
-#define SYMBOL_MENU_ASSIGNATURE 'A'
-#define SYMBOL_MENU_CALC 'M'
-#define SYMBOL_FINALIZE '!'
-#define SYMBOL_QUIT '0'
-
-#define MSG_WELCOME "Bienvenidx"
