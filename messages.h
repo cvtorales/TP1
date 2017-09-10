920e3a069a6b429b840fd2acb5e940b484009fc3
fedefaganelli <31784079+fedefaganelli@users.noreply.github.com>
2017-09-10 19:32:10 -0300

    Add files via upload

---
+++ messages.h
@@ -0,0 +1,34 @@
+#define MSG_WELCOME "Bienvenidx"
+
+#define MSG_WELCOME_MENU_PPAL "Elija que desea hacer"
+#define MSG_OPTION_1_MENU_PPAL "Modificar registro personal"
+#define MSG_OPTION_2_MENU_PPAL "Modificar asignaturas"
+#define MSG_OPTION_3_MENU_PPAL "Calcular metrica individual"
+#define MSG_OPTION_4_MENU_PPAL "Finalizar"
+#define MSG_OPTION_5_MENU_PPAL "Salir"
+
+#define SYMBOL_MENU_PERSONAL_REGISTER 'R'
+#define SYMBOL_MENU_ASSIGNATURE 'A'
+#define SYMBOL_MENU_CALC 'M'
+#define SYMBOL_FINALIZE '!'
+#define SYMBOL_QUIT '0'
+
+#define SYMBOL_SUBMENU_ASSIG_ADD_ASSIG '+'
+#define SYMBOL_SUBMENU_ASSIG_BACK '0'
+
+#define MSG_WELCOME_MENU_CALC "Metricas"
+#define SYMBOL_SUBMENU_CALC_MEDIA 'P'
+#define SYMBOL_SUBMENU_CALC_MIN 'm'
+#define SYMBOL_SUBMENU_CALC_MAX 'M'
+#define SYMBOL_SUBMENU_CALC_QUANTITY '#'
+#define SYMBOL_SUBMENU_CALC_FAILURES '2'
+#define SYMBOL_SUBMENU_CALC_BACK '0'
+#define MSG_OPTION_SUBMENU_CALC_MEDIA "Promedio"
+#define MSG_OPTION_SUBMENU_CALC_MIN "Minimo"
+#define MSG_OPTION_SUBMENU_CALC_MAX "Maximo"
+#define MSG_OPTION_SUBMENU_CALC_QUANTITY "Cantidad de materias"
+#define MSG_OPTION_SUBMENU_CALC_FAILURES "Aplazos"
+#define MSG_OPTION_SUBMENU_CALC_BACK "Volver"
+
+#define MAX_ROW 10
+#define MAX_COL 30
