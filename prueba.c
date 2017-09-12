5271731c45d77df580cc7b7face97a1d7d3f48c5
agustinpiperno <31783815+agustinpiperno@users.noreply.github.com>
2017-09-12 17:13:25 -0300

    funcion para agregar padron con sus validaciones
    falta agregar macros de error

--- prueba.c
+++ prueba.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main(void)
-{	
+	
+long int add_padron(void)
+{
 	size_t aux;
 
-	int num;
+	int padron;
 	char *temp;
 
 	char line[100];
@@ -13,26 +14,22 @@ int main(void)
 	fgets(line,100,stdin);
 	aux=strlen(line);
 
-
-	if( aux != 6 && aux != 7){
-		printf("%s\n", "mal");
+	if( aux != 6 && aux != 7)
+	{
+		printf("%s\n", "mal"); /* poner un mensaje de error */
 		return EXIT_FAILURE;
 	}
 
 	num=strtol(line, &temp, 10);	  
-	if(*temp && *temp != '\n') {/* entra cuando temp se queda apuntando a una letra */
+	if(*temp && *temp != '\n') 
+	{/* entra cuando temp se queda apuntando a una letra */
 		printf("%s\n", "mal");
 		return EXIT_FAILURE;  
 	}
-	if (num < 0){
+	if (num < 0)
+	{
 		printf("%s\n", "mal");
 		return EXIT_FAILURE; 
 	}
-
-
-	printf("%i\n", num);
-
-
-	return 0;
-
+	return padron;
 }
