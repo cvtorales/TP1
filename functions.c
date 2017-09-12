a26ecc48730b4ab8f8b10a4a986358a037716257
agustinpiperno <31783815+agustinpiperno@users.noreply.github.com>
2017-09-12 17:22:15 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -196,3 +196,35 @@ void add_career(char m[MAX_ROW_PERS_REG][MAX_COL_PERS_REG])
 	}
 
 }
+
+long int add_padron(void)
+{
+	size_t aux;
+
+	int padron;
+	char *temp;
+
+	char line[100];
+
+	fgets(line,100,stdin);
+	aux=strlen(line);
+
+	if( aux != 6 && aux != 7)
+	{
+		printf("%s\n", "mal"); /* poner un mensaje de error */
+		return EXIT_FAILURE;
+	}
+
+	num=strtol(line, &temp, 10);	  
+	if(*temp && *temp != '\n') 
+	{/* entra cuando temp se queda apuntando a una letra */
+		printf("%s\n", "mal");
+		return EXIT_FAILURE;  
+	}
+	if (num < 0)
+	{
+		printf("%s\n", "mal");
+		return EXIT_FAILURE; 
+	}
+	return padron;
+}
